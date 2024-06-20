664acfcb10319389e4263f50f9b579b3f1f9f83c
yash2324 <yg232004@gmail.com>
2024-06-20 19:48:04 +0530

    changes to inital files

---
+++ src/app/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <div className="px-2 py-4 border-b flex justify-between items-center">
+      <div className="heading text-3xl">Header</div>
+      <button className="bg-black text-white py-5 px-3">Sign in </button>
+    </div>
+  );
+};
+
+export default Header;
