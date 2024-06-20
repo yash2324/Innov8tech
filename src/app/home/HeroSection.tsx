2de5a5db978ffad48e4837c18dd1c000b617e1af
yash2324 <yg232004@gmail.com>
2024-06-20 20:49:33 +0530

    finished herosection

--- src/app/home/HeroSection.tsx
+++ src/app/home/HeroSection.tsx
@@ -5,9 +5,13 @@ type Props = {};
 const HeroSection = (props: Props) => {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-12 p-10">
-      <div className="col-span-7">
-        <h1 className="text-2xl sm:text-4xl">Tech Updates for busy people</h1>
-        <button>Browse Articles</button>
+      <div className="col-span-7 w-full place-self-center text-center sm:text-left justify-self-start">
+        <h1 className="text-2xl sm:text-4xl md:text-6xl md:leading-normal">
+          Tech updates for busy people
+        </h1>
+        <button className="bg-indigo-500 text-white px-4 py-2 border-2 rounded shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)]">
+          Browse Articles
+        </button>
       </div>
       <div className="col-span-5">
         <Image
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,13 @@ const config: Config = {
     },
     container: {
       center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1240px",
+        "2xl": "1300px",
+      },
     },
   },
   plugins: [],
