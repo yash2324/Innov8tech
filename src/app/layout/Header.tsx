43bd5ce927603ea55fdede4ae10517015fdf8773
yash2324 <yg232004@gmail.com>
2024-06-21 20:40:07 +0530

    changed btn

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header = (props: Props) => {
   return (
     <div className="px-2 py-4 border-b flex justify-between items-center">
       <div className="heading text-3xl">Header</div>
-      <button className="bg-black text-white py-5 px-3">Sign in </button>
+      <button className="bg-black text-white py-5 px-3">Sign up </button>
     </div>
   );
 };
