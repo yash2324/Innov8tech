3b8b654fc27b19c44b783c81280309bb4cb1cd64
yash2324 <yg232004@gmail.com>
2024-06-23 23:55:14 +0530

    updating header

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className="px-2 py-4 border-b flex justify-between items-center">
-      <div className="heading text-3xl">Header</div>
+      <div className="heading text-3xl">Yash's Blog</div>
       <button className="bg-black text-white py-5 px-3">Sign up </button>
     </div>
   );
