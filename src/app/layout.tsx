e534ef4cb00b8709e3d4b3ece7ec256027b34ed8
yash2324 <yg232004@gmail.com>
2024-06-25 09:17:26 +0530

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         <main className="container mb-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/app/layout/Footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import githubIcon from "../../../public/github-icon.svg";
+import twitterIcon from "../../../public/twitter-icon.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <div className="bg-gray-800 text-white py-12 px-4">
+      <div className="flex container justify-between">
+        <div className="flex flex-row items-center">
+          <div>
+            <div className="text-2xl mb-3">Yash Gupta</div>
+            <div className="text-sm">© 20234 Yash Gupta</div>
+          </div>
+        </div>
+        <div className="flex">
+          <a
+            href="https://github.com/yash2324"
+            target="_blank"
+            className="mr-3"
+          >
+            <Image src={githubIcon} alt="github" width={32} height={32} />
+          </a>
+          <a href="https://twitter.com/yashgupta023" target="_blank">
+            <Image src={twitterIcon} alt="twitter" width={32} height={32} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
