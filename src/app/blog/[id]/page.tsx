0db7b5b4227be7ce99e2ddeada62e50c2707c884
yash2324 <yg232004@gmail.com>
2024-06-25 07:08:43 +0530

    created blog page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-
+import { prisma } from "../../../../lib/prisma";
 type Props = {};
 
-const page = ({
+const page = async ({
   params,
 }: {
   params: {
     id: string;
   };
 }) => {
-  return <div>page : {params.id}</div>;
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+  console.log(post);
+  return (
+    <div>
+      {post && (
+        <>
+          <h1 className="text-5xl mb-5">{post.title}</h1>
+          <div className="flex items-center">
+            <p className="text-sm text-gray-500">By</p>
+            <div className="w-5 h-5 ml-3 mr-2 rounded-full bg-contain"></div>
+            {/* <p className="text-sm text-gray-500">{post.author.name}</p> */}
+          </div>
+          <p className="mt-4">{post.content}</p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default page;
--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -5,7 +6,9 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <div className="px-2 py-4 border-b flex justify-between items-center">
-      <div className="heading text-3xl">Yash's Blog</div>
+      <Link href={"/"}>
+        <div className="heading text-3xl">Yash's Blog</div>
+      </Link>
       <button className="bg-black text-white py-5 px-3">Sign up </button>
     </div>
   );
